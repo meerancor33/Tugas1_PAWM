0183c2c42bf8c4e858e4120075b9ff1762522679
meerancor33 <18223126@std.stei.itb.ac.id>
2025-10-12 09:08:02 +0700

    integrate to external backend

--- backend/app.py
+++ backend/app.py
@@ -298,4 +298,4 @@ def learning_action(
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=int(os.getenv("PORT", 8000)))
